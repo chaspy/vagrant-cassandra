c952319e9e7045908f057cb72487ca3681884414
kondo takeshi <chaspy@users.noreply.github.com>
2019-06-18 01:34:19 +0900

    Set up Cassandra
    ref: https://www.vultr.com/docs/how-to-install-apache-cassandra-3-11-x-on-ubuntu-16-04-lts

--- init.sh
+++ init.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 sudo apt-get update -y
+
+# ref: https://www.vultr.com/docs/how-to-install-apache-cassandra-3-11-x-on-ubuntu-16-04-lts
+
+# Install Java
+sudo apt install openjdk-8-jre -y
+
+# Set JAVA_HOME
+echo "JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")" | sudo tee -a /etc/profile
+source /etc/profile
+echo $JAVA_HOME
+
+# Install Python2
+sudo apt install python -y
+
+# Install Cassandra
+echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install -y cassandra
+
+# Test the installation of Cassandra
+sudo service cassandra start
